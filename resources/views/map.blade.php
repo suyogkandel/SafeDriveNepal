e9e388383ec8f81cc72b63b6809e4c1b9400dbcd
suyogkandel <suyogkandel@gmail.com>
2023-06-18 09:12:46 +0545

    final commit

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -27,34 +27,6 @@ var busIcon = L.icon({
     iconSize: [30,30]
 })
 
-//marker
-var marker = L.marker([27.66882, 84.43168],{icon: busIcon}).addTo(map);
-
-//map click event
-map.on('click',function(e) {
-    console.log(e)
-    // adding lat and long 
-    var secondMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-    L.Routing.control({
-            waypoints: [
-                // L.latLng(27.707771, 85.319564),
-                L.latLng(27.66882, 84.43168),
-    // second location is from the click
-                //L.latLng(e.latlng.lat, e.latlng.lng)
-                L.latLng(27.69197, 84.43716)
-
-            ]
-    }).on('routesfound', function(e){
-        console.log(e)
-        e.routes[0].coordinates.forEach(function(coord, index){
-            setTimeout(() => {
-                marker.setLatLng([coord.lat, coord.lng])
-            }, 700 * index); // every 100 millisec times index change the marker|
-        })
-    })
-    
-    .addTo(map);
-})
 
 
 //marker
@@ -79,7 +51,43 @@ map.on('click',function(e) {
         e.routes[0].coordinates.forEach(function(coord, index){
             setTimeout(() => {
                 marker.setLatLng([coord.lat, coord.lng])
-            }, 600 * index); // every 100 millisec times index change the marker|
+            }, 750 * index); // every 100 millisec times index change the marker|
+        })
+    })
+    
+    .addTo(map);
+})
+
+//marker icon
+var busIcon2=L.icon({
+    iconUrl: "/images/bus2.png",
+    iconSize: [30,30]
+})
+//marker
+var marker = L.marker([27.66882, 84.43168],{icon: busIcon2}).addTo(map);
+
+//map click event
+map.on('click',function(e) {
+    console.log(e)
+    // adding lat and long 
+    var secondMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    L.Routing.control({
+            waypoints: [
+                // L.latLng(27.707771, 85.319564),
+                L.latLng(27.66882, 84.43168),
+    // second location is from the click
+                // L.latLng(e.latlng.lat, e.latlng.lng)
+                // L.latLng(27.67056, 84.43370)
+                L.latLng(27.671119517468338, 84.43552127072529)
+
+            ]
+    }).on('routesfound', function(e){
+        console.log( e.routes[0].coordinates)
+        e.routes[0].coordinates.forEach(function(coord, index){
+            setTimeout(() => {
+                // console.log("HERE",coord.lat, coord.lng)
+                marker.setLatLng([coord.lat, coord.lng])
+            }, 680 * index); // every 100 millisec times index change the marker|
         })
     })
     
